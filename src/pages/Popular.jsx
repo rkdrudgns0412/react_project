13985fa5080cabe69d829c45a49061abfdc0f0e9
rkdrudgns0412 <rkdrudgns8233@gmail.com>
2024-11-18 17:14:20 +0900

    feat: popular.jsx 데이터 넣기

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Popular() {
-  return (
-    <div>Popular</div>
-  )
+  const baseURL = "https://api.themoviedb.org/3/movie";
+  const path = "/popular";
+  const params = new URLSearchParams({
+    api_key: "56daa931cc7f953de7359b43531fa672",
+    language: "ko",
+  });
+
+  const URL = `${baseURL}${path}?${params}`;
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const reponse = await axios.get(URL);
+
+        let data = reponse.data;
+
+        data = data.results;
+
+        const dataimg = data.map((el) => {
+          const imgs = `https://image.tmdb.org/t/p/w500${el.backdrop_path}`;
+          return (
+            <div>
+              <img src={imgs} alt="" />
+              <div>{el.title}</div>
+            </div>
+          );
+        });
+
+        setPosts(dataimg);
+      } catch {}
+    }
+    fetchPost();
+  }, []);
+
+  console.log(posts);
+
+  return <div>{posts}</div>;
 }
